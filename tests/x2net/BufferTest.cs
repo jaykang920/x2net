073d680c500efc456e011befd794e3fe5699bb44
Jae-jun Kang <jaykang920@gmail.com>
2017-07-14 17:31:45 +0900

    added BufferTest.TestTrim

--- tests/x2net/BufferTest.cs
+++ tests/x2net/BufferTest.cs
@@ -32,5 +32,40 @@ namespace x2net.tests
             
             //  
         }
+
+        [Fact]
+        public void TestTrim()
+        {
+            var buf = new x2net.Buffer();
+
+            buf.EnsureCapacityToWrite(buf.BlockSize);  // numBlocks + 1
+
+            buf.Position = 24;
+
+            // front <= pos
+            buf.Trim();
+
+            Assert.Equal(buf.BlockSize - 24, buf.Length);
+            Assert.Equal(0, buf.Position);
+
+            buf.Position = (int)buf.Length;
+
+            // reset
+            buf.Trim();
+
+            Assert.Equal(0, buf.Length);
+            Assert.Equal(0, buf.Position);
+
+            buf.EnsureCapacityToWrite(buf.BlockSize);
+
+            buf.MarkToRead(1024);
+            buf.Position = 24;
+
+            // front <= marker
+            buf.Trim();
+
+            Assert.Equal(buf.BlockSize - 1024, buf.Length);
+            Assert.Equal(0, buf.Position);
+        }
     }
 }
